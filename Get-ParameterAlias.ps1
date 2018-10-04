86a8ddf60319c8fcbf199e250e21959b6b4318ea
Shane O'Neill <shaneoneillis@gmail.com>
2018-10-04 14:01:50 +0100

    Create Get-ParameterAlias.ps1

---
+++ Get-ParameterAlias.ps1
@@ -0,0 +1,30 @@
+function Get-ParameterAlias {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0)]
+        [Alias('Function')]
+        [String]
+        $Command
+    )
+    
+    begin {
+    }
+    
+    process {
+        Write-Verbose -Message "[PROCESS] Checking parameter aliases for [$_]..."
+        foreach ($cmd in (Get-Command -Name $Command).Name) {
+            $cmdObject = Get-Command -Name $cmd
+            $cmdObject.Parameters.Values |
+                ForEach-Object -Process {
+                    [PSCustomObject]@{
+                        Command = $cmd
+                        Parameterr = $_.Name
+                        Alias = $_ | Select-Object -ExpandProperty Aliases
+                    }
+                }
+        }
+    }
+    
+    end {
+    }
+}
