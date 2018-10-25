8a900971cd5996cb26b723788fa44f8d64a7b080
Shaneis <shaneoneillis@gmail.com>
2018-10-25 21:55:13 +0100

    Forgot to actually show the report.

--- Invoke-SqlUndercoverCollection.ps1
+++ Invoke-SqlUndercoverCollection.ps1
@@ -243,6 +243,10 @@ function Invoke-SQLUndercoverCollection {
             $CentralConnection.Query($ReportQry)
             
             Write-Verbose "[PROCESS] [$CentralServer] - SQLUndercover Report has completed."
+            $FinalReportQry = "SELECT TOP (1) ReportDat FROM [$LoggingDb].[Inspector].[ReportData] ORDER BY ID DESC;"
+            $Report = $CentralConnection.Query($FinalReportQry)
+
+            $Report | Format-Table -Wrap -HideTableHeaders | Out-File -FilePath $FileName -Width 4000
         }
     }
     end {
