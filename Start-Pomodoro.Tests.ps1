604784923a1773f8dda598c49aedc20fbc3a146d
Shaneis <shaneoneillis@gmail.com>
2019-03-24 22:48:01 +0000

    Added subjects file, Worked on tests, and function.

--- Start-Pomodoro.Tests.ps1
+++ Start-Pomodoro.Tests.ps1
@@ -1,9 +1,27 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\$sut"
+. "$here\$sut"sd
 
-Describe "Start-Pomodoro" {
-    It "does something useful" {
-        $true | Should -Be $false
+Describe 'Start-Pomodoro' {
+    Context 'Properties' {
+        BeforeEach -Scriptblock {
+            $basicExecution = Start-Pomodoro
+        }
+
+        $expectedProperites = 'Timing', 'Subject'
+        foreach ($expProperty in $expectedProperites) {
+            It "has the expected property of [$expProperty]" {
+                $notePropertyName = ($basicExecution | Get-Member -MemberType NoteProperty).Name
+                $notePropertyName | Should -Contain $expProperty
+            }
+        }
+    }
+
+    Context 'Timings' {
+        It 'has the first timing of 25' {
+            $Pomodoro = Start-Pomodoro
+            
+            $Pomodoro[0].Timing | Should -Be 25
+        }
     }
 }
--- Start-Pomodoro.ps1
+++ Start-Pomodoro.ps1
@@ -5,48 +5,15 @@
     process {
         for([Int]$i = 0; $i -lt 10; $i++) {
             $Timing = switch ($i) {
-                {($_ % 8) -eq 0 -and $_ -ne 0} {
-                    10
-                    break
-                }
-                {($_ % 2) -eq 0 -and $_ -ne 0} {
-                    5
-                    break
-                }
-                default {
-                    25
-                }
+                {($_ % 8) -eq 0 -and $_ -ne 0} {10; break}
+                {($_ % 2) -eq 0 -and $_ -ne 0} {5; break}
+                default {25}
             }
 
             [PSCustomObject]@{
                 Timing = $Timing
                 Subject = $null
             }
-        }
-    }
-}
-
-
-Describe 'Start-Pomodoro' {
-    Context 'Properties' {
-        BeforeEach -Scriptblock {
-            $basicExecution = Start-Pomodoro
-        }
-
-        $expectedProperites = 'Timing', 'Subject'
-        foreach ($expProperty in $expectedProperites) {
-            It "has the expected property of [$expProperty]" {
-                $notePropertyName = ($basicExecution | Get-Member -MemberType NoteProperty).Name
-                $notePropertyName | Should -Contain $expProperty
-            }
-        }
-    }
-
-    Context 'Timings' {
-        It 'has the first timing of 25' {
-            $Pomodoro = Start-Pomodoro
-            
-            $Pomodoro[0].Timing | Should -Be 25
         }
     }
 }
