aa678ee74b6dfa42236f6c19113ee776c74d7b03
Shaneis <shaneoneillis@gmail.com>
2019-04-27 22:49:23 +0100

    [Bug Fix]: Removed unintential characters after dot-source.

--- Start-Pomodoro.Tests.ps1
+++ Start-Pomodoro.Tests.ps1
@@ -1,6 +1,6 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\$sut"sd
+. "$here\$sut"
 
 Describe 'Start-Pomodoro' {
     Context 'Properties' {
